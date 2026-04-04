aa732cbb181e673638e17ceb14aa67f8f8f0afb4
hidemasa <176674127+tachikoma3@users.noreply.github.com>
2026-04-04 18:17:43 +0900

    Add files via upload

--- form/about.js
+++ form/about.js
@@ -8,3 +8,15 @@ gsap.from(".about__inner", {
   duration: 0.6,
   ease: "power3.out"
 });
+
+gsap.from(".about__img", {
+  scrollTrigger: {
+    trigger: ".about__inner",
+    start: "top 40%"
+  },
+  stagger: 0.5,
+  opacity: 0,
+  y: 20,
+  duration: 0.6,
+  ease: "power3.out"
+});
--- form/hero.js
+++ form/hero.js
@@ -3,15 +3,27 @@ gsap.from(".hero__title",  {
   start: "top 40%",
   once: true,
   opacity: 0,
-  y: 24,
+  x: -24,
   duration: 0.8,
   ease: "power3.out"
 });
+
+gsap.from(".hero__img",  {
+  trigger: ".hero",
+  start: "top 40%",
+  once: true,
+  opacity: 0,
+  x: 24,
+  duration: 0.8,
+  ease: "power3.out"
+});
+
 
   gsap.from(".hero__text",  {
   trigger: ".hero",
   start: "top 40%",
   once: true,
+  stagger:0.2,
   opacity: 0,
   y: 24,
   duration: 0.8,
